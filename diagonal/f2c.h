ec61a562de88d62cb80684d37e9a331338307c25
JP <jpmedinagl@gmail.com>
2024-05-24 12:11:00 -0400

    changing name

--- diagonal/f2c.h
+++ diagonal/f2c.h
@@ -13,8 +13,8 @@ typedef char *address;
 typedef short int shortint;
 typedef float real;
 typedef double doublereal;
-typedef struct { real r, i; } complex;
-typedef struct { doublereal r, i; } doublecomplex;
+// typedef struct { real r, i; } complex;
+// typedef struct { doublereal r, i; } doublecomplex;
 typedef long int logical;
 typedef short int shortlogical;
 typedef char logical1;
@@ -131,8 +131,8 @@ union Multitype {	/* for multiple entry points */
 	/* longint j; */
 	real r;
 	doublereal d;
-	complex c;
-	doublecomplex z;
+	// complex c;
+	// doublecomplex z;
 	};
 
 typedef union Multitype Multitype;
