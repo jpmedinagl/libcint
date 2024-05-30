7f1cf1d73c214c8137768cf077b7ea3f5e72b7d5
JP <jpmedinagl@gmail.com>
2024-05-30 16:44:54 -0400

    compiling main

--- librint/src/main.rs
+++ librint/src/main.rs
@@ -69,14 +69,6 @@ fn main() -> io::Result<()> {
     let mut bas_arr: [i32; nbas * BAS_SLOTS] = [0, 0, 3, 1, 0, 28, 31, 0, 1, 0, 3, 1, 0, 28, 31, 0];
     let mut env_arr: [f64; 34] = [0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 
         -1.5117809, 0., 0., 0., 1.5117809, 0., 3.42525091, 0.62391373, 0.1688554, 0.98170675, 0.94946401, 0.29590645];
-    
-    for i in 0..(natm * ATM_SLOTS) {
-        println!("{}", atm_arr[i]);
-    }
-
-    for i in 0..(34) {
-        println!("{}", env_arr[i]);
-    }
 
     let mut shls_arr: [i32; 4] = [0, 0, 0, 0];
     let mut opt: *mut CINTOpt = todo!();
@@ -87,10 +79,8 @@ fn main() -> io::Result<()> {
             shls_arr[0] = i as i32;
             shls_arr[1] = j as i32;
             
-            unsafe {
             let di = CINTcgto_cart(i, &bas_arr);
             let dj = CINTcgto_cart(j, &bas_arr);
-            }
 
             let mut buf = vec![0.0; (di * dj) as usize];
 
