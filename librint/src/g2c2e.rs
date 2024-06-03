c4439c77afdfef4e69ea8ed4a90f8e51e26d0087
JP <jpmedinagl@gmail.com>
2024-06-03 16:44:12 -0400

    fixing the C extern

--- librint/src/g2c2e.rs
+++ librint/src/g2c2e.rs
@@ -1,132 +1,14 @@
 #![allow(dead_code, mutable_transmutes, non_camel_case_types, non_snake_case, non_upper_case_globals, unused_assignments, unused_mut)]
-extern "C" {
-    fn CINTg0_2e(
-        g: *mut libc::c_double,
-        rij: *mut libc::c_double,
-        rkl: *mut libc::c_double,
-        cutoff: libc::c_double,
-        envs: *mut CINTEnvVars,
-    ) -> libc::c_int;
-    fn CINTg0_2e_2d(g: *mut libc::c_double, bc: *mut Rys2eT, envs: *mut CINTEnvVars);
-    fn CINTg0_2e_2d4d_unrolled(
-        g: *mut libc::c_double,
-        bc: *mut Rys2eT,
-        envs: *mut CINTEnvVars,
-    );
-    fn CINTsrg0_2e_2d4d_unrolled(
-        g: *mut libc::c_double,
-        bc: *mut Rys2eT,
-        envs: *mut CINTEnvVars,
-    );
-    fn CINTcommon_fac_sp(l: libc::c_int) -> libc::c_double;
-}
-#[derive(Copy, Clone)]
-#[repr(C)]
-pub struct PairData {
-    pub rij: [libc::c_double; 3],
-    pub eij: libc::c_double,
-    pub cceij: libc::c_double,
-}
-#[derive(Copy, Clone)]
-#[repr(C)]
-pub struct CINTOpt {
-    pub index_xyz_array: *mut *mut libc::c_int,
-    pub non0ctr: *mut *mut libc::c_int,
-    pub sortedidx: *mut *mut libc::c_int,
-    pub nbas: libc::c_int,
-    pub log_max_coeff: *mut *mut libc::c_double,
-    pub pairdata: *mut *mut PairData,
-}
-#[derive(Copy, Clone)]
-#[repr(C)]
-pub struct CINTEnvVars {
-    pub atm: *mut libc::c_int,
-    pub bas: *mut libc::c_int,
-    pub env: *mut libc::c_double,
-    pub shls: *mut libc::c_int,
-    pub natm: libc::c_int,
-    pub nbas: libc::c_int,
-    pub i_l: libc::c_int,
-    pub j_l: libc::c_int,
-    pub k_l: libc::c_int,
-    pub l_l: libc::c_int,
-    pub nfi: libc::c_int,
-    pub nfj: libc::c_int,
-    pub c2rust_unnamed: C2RustUnnamed_1,
-    pub c2rust_unnamed_0: C2RustUnnamed_0,
-    pub nf: libc::c_int,
-    pub rys_order: libc::c_int,
-    pub x_ctr: [libc::c_int; 4],
-    pub gbits: libc::c_int,
-    pub ncomp_e1: libc::c_int,
-    pub ncomp_e2: libc::c_int,
-    pub ncomp_tensor: libc::c_int,
-    pub li_ceil: libc::c_int,
-    pub lj_ceil: libc::c_int,
-    pub lk_ceil: libc::c_int,
-    pub ll_ceil: libc::c_int,
-    pub g_stride_i: libc::c_int,
-    pub g_stride_k: libc::c_int,
-    pub g_stride_l: libc::c_int,
-    pub g_stride_j: libc::c_int,
-    pub nrys_roots: libc::c_int,
-    pub g_size: libc::c_int,
-    pub g2d_ijmax: libc::c_int,
-    pub g2d_klmax: libc::c_int,
-    pub common_factor: libc::c_double,
-    pub expcutoff: libc::c_double,
-    pub rirj: [libc::c_double; 3],
-    pub rkrl: [libc::c_double; 3],
-    pub rx_in_rijrx: *mut libc::c_double,
-    pub rx_in_rklrx: *mut libc::c_double,
-    pub ri: *mut libc::c_double,
-    pub rj: *mut libc::c_double,
-    pub rk: *mut libc::c_double,
-    pub c2rust_unnamed_1: C2RustUnnamed,
-    pub f_g0_2e: Option::<unsafe extern "C" fn() -> libc::c_int>,
-    pub f_g0_2d4d: Option::<unsafe extern "C" fn() -> ()>,
-    pub f_gout: Option::<unsafe extern "C" fn() -> ()>,
-    pub opt: *mut CINTOpt,
-    pub idx: *mut libc::c_int,
-    pub ai: [libc::c_double; 1],
-    pub aj: [libc::c_double; 1],
-    pub ak: [libc::c_double; 1],
-    pub al: [libc::c_double; 1],
-    pub fac: [libc::c_double; 1],
-    pub rij: [libc::c_double; 3],
-    pub rkl: [libc::c_double; 3],
-}
-#[derive(Copy, Clone)]
-#[repr(C)]
-pub union C2RustUnnamed {
-    pub rl: *mut libc::c_double,
-    pub grids: *mut libc::c_double,
-}
-#[derive(Copy, Clone)]
-#[repr(C)]
-pub union C2RustUnnamed_0 {
-    pub nfl: libc::c_int,
-    pub ngrids: libc::c_int,
-}
-#[derive(Copy, Clone)]
-#[repr(C)]
-pub union C2RustUnnamed_1 {
-    pub nfk: libc::c_int,
-    pub grids_offset: libc::c_int,
-}
-#[derive(Copy, Clone)]
-#[repr(C)]
-pub struct Rys2eT {
-    pub c00x: [libc::c_double; 32],
-    pub c00y: [libc::c_double; 32],
-    pub c00z: [libc::c_double; 32],
-    pub c0px: [libc::c_double; 32],
-    pub c0py: [libc::c_double; 32],
-    pub c0pz: [libc::c_double; 32],
-    pub b01: [libc::c_double; 32],
-    pub b00: [libc::c_double; 32],
-    pub b10: [libc::c_double; 32],
-}
+
+use crate::g1e::CINTcommon_fac_sp;
+use crate::g2e::CINTg0_2e;
+use crate::g2e::CINTg0_2e_2d;
+use crate::g2e::CINTg0_2e_2d4d_unrolled;
+use crate::g2e::CINTsrg0_2e_2d4d_unrolled;
+
+use crate::cint::CINTEnvVars;
+use crate::g2e::Rys2eT;
+
 #[no_mangle]
 pub unsafe extern "C" fn CINTinit_int2c2e_EnvVars(
     mut envs: *mut CINTEnvVars,
