cf5593a3d46637889226562f24d1da3d2802891a
JP <jpmedinagl@gmail.com>
2024-06-11 15:58:30 -0400

    removing comments

--- librint/faer/eigen.rs
+++ librint/faer/eigen.rs
@@ -2,9 +2,6 @@ extern crate librint;
 
 use faer::mat;
 use faer::linalg::solvers::Eigendecomposition;
-// use faer::ComplexE;
-// use faer::types::{c64, ComplexField};
-// use faer::types::c64;/
 use faer::complex_native::c64;
 
 fn main() {
@@ -13,31 +10,26 @@ fn main() {
     let S = mat::from_column_major_slice::<f64>(&a, 2, 2);
 
     // Perform eigendecomposition
-    let eig_decomp: Eigendecomposition<c64> = Eigendecomposition::new_from_real(S); // Unwrap here is safe for demonstration purposes
+    let eig_decomp: Eigendecomposition<c64> = Eigendecomposition::new_from_real(S);
 
-    // // Extract eigenvalues and eigenvectors
+    // Extract eigenvalues and eigenvectors
     let eigenvalues = eig_decomp.s();
     let eigenvectors = eig_decomp.u();
 
-
-    // println!("Eigenvalues:");
-    // for val in eigenvalues.iter() {
-    //     println!("{:>8.3} ", val);
-    // }
-
-    // Print the eigenvectors
-    println!("Eigenvectors:");
-    for i in 0..eigenvectors.ncols() {
-        for j in 0..eigenvectors.nrows() {
-            print!("{:>8.3} ", eigenvectors.read(j, i).re());
-        }
-        println!();
-    }
-
+    println!("Eigenvalues");
     let eign = eigenvalues.column_vector();
-
     for i in 0..eign.nrows() {
         print!("{} ", eign.read(i).re());
     }
     println!();
+
+
+    // Print the eigenvectors
+    println!("Eigenvectors:");
+    for i in 0..eigenvectors.nrows() {
+        for j in 0..eigenvectors.ncols() {
+            print!("{:>8.3} ", eigenvectors.read(i, j).re());
+        }
+        println!();
+    }
 }
