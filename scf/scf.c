39a34e962c9dbe1c08d8f1f95871790163e16518
JP <jpmedinagl@gmail.com>
2024-07-19 15:08:46 -0400

    adding generic integral functions

--- scf/scf.c
+++ scf/scf.c
@@ -9,24 +9,41 @@
 
 #define N_STO 3
 
+extern int enzyme_dup;
+extern int enzyme_out;
+extern int enzyme_const;
+
 int __enzyme_autodiff(void *, ...);
 
+typedef int (* int1e)(double *buf, int *shls, 
+					int *atm, int natm, int *bas, int nbas, double *env);
+
+typedef int (* int2e)(double *buf, int *shls, 
+					int *atm, int natm, int *bas, int nbas, double *env, CINTOpt *opt);
+
+typedef FINT (* cgto)(const FINT bas_id, const FINT *bas);
+
 int cint1e_ovlp_cart(double *buf, int *shls,
+                    int *atm, int natm, int *bas, int nbas, double *env);
+
+int cint1e_ovlp_sph(double *buf, int *shls, 
                     int *atm, int natm, int *bas, int nbas, double *env);
 
 int cint1e_kin_cart(double *buf, int *shls,
                     int *atm, int natm, int *bas, int nbas, double *env);
 
+int cint1e_kin_sph(double *buf, int *shls,
+                    int *atm, int natm, int *bas, int nbas, double *env);
+
 int cint1e_nuc_cart(double *buf, int *shls,
+                    int *atm, int natm, int *bas, int nbas, double *env);
+
+int cint1e_nuc_sph(double *buf, int *shls,
                     int *atm, int natm, int *bas, int nbas, double *env);
 
 extern int dsyev_(char *jobz, char *uplo, integer *n, doublereal *a, 
     integer *lda, doublereal *w, doublereal *work, integer *lwork, 
 	integer *info);
-
-extern int enzyme_dup;
-extern int enzyme_out;
-extern int enzyme_const;
 
 double * c_arr(int x, int y) 
 {
@@ -103,6 +120,129 @@ void read_arrays(FILE * file, int natm, int nbas, int ** atm, int ** bas, double
 		count++;
 	}
 	fclose(file);
+}
+
+double * integral1e(int natm, int nbas, int nshells, int * atm, int * bas, double * env, int coord, int type) {
+    double * R = c_arr(nshells, nshells);
+
+    int1e func;
+    cgto intcgto;
+
+    if (coord == 0) {
+        // CARTERSIAN
+        intcgto = CINTcgto_cart;
+
+        if (type == 0) {
+            func = cint1e_ovlp_cart;
+        } else if (type == 1) {
+            func = cint1e_kin_cart;
+        } else if (type == 2) {
+            func = cint1e_nuc_cart;
+        }
+    } else if (coord == 1) {
+        // SPHERIC
+        intcgto = CINTcgto_spheric;
+
+        if (type == 0) {
+            func = cint1e_ovlp_sph;
+        } else if (type == 1) {
+            func = cint1e_kin_sph;
+        } else if (type == 2) {
+            func = cint1e_nuc_sph;
+        }
+    }
+
+    int mu, nu;
+    int di, dj;
+    int shls[4];
+    int c;
+    double * buf;
+
+    mu = 0;
+    for (int i = 0; i < nbas; i++) {
+        nu = 0;
+        for (int j = 0; j < nbas; j++) {
+            shls[0] = i; di = intcgto(i, bas);
+            shls[1] = j; dj = intcgto(j, bas);
+
+            buf = malloc(sizeof(double) * di * dj);
+            func(buf, shls, atm, natm, bas, nbas, env);
+
+            c = 0;
+            for (int nuj = nu; nuj < nu + dj; nuj++) {
+                for (int mui = mu; mui < mu + di; mui++) {
+                    R[mui * nshells + nuj] = buf[c];
+                    c++;
+                }
+            }
+            nu += dj;
+        }
+        mu += di;
+    }
+
+    return R;
+}
+
+double * integral2e(int natm, int nbas, int nshells, int * atm, int * bas, double * env, int coord) {
+    double * R = c_arr_doub(nshells, nshells, nshells, nshells);
+
+    int2e func;
+    cgto intcgto;
+
+    if (coord == 0) {
+        intcgto = CINTcgto_cart;
+        func = cint2e_cart;
+    } else if (coord == 1) {
+        intcgto = CINTcgto_spheric;
+        func = cint2e_sph;
+    }
+    
+    int mu, nu, sig, lam;
+    int di, dj, dk, dl;
+    int shls[4];
+    int c;
+    double * buf;
+
+    mu = 0;
+    for (int i = 0; i < nbas; i++) {
+        nu = 0;
+        for (int j = 0; j < nbas; j++) {
+            sig = 0;
+            for (int k = 0; k < nbas; k++) {
+                lam = 0;
+                for (int l = 0; l < nbas; l++) {
+                    shls[0] = i; di = intcgto(i, bas);
+                    shls[1] = j; dj = intcgto(j, bas);
+                    shls[2] = k; dk = intcgto(k, bas);
+                    shls[3] = l; dl = intcgto(l, bas);
+
+                    buf = malloc(sizeof(double) * di * dj * dk * dl);
+                    func(buf, shls, atm, natm, bas, nbas, env, NULL);
+
+                    c = 0;
+                    for (int laml = lam; laml < lam + dl; laml++) {
+                        for (int sigk = sig; sigk < sig + dk; sigk++) {
+                            for (int nuj = nu; nuj < nu + dj; nuj++) {
+                                for (int mui = mu; mui < mu + di; mui++) {
+                                    R[(int) (mui*pow(nshells, 3) + nuj*pow(nshells, 2) + sigk*nshells + laml)] = buf[c];
+                                    c++;
+                                }
+                            }
+                        }
+                    }
+
+                    free(buf);
+
+                    lam += dl;
+                }
+                sig += dk;
+            }
+            nu += dj;
+        }
+        mu += di;
+    }
+
+    return R;
 }
 
 void integrals(int natm, int nbas, int nshells, int * atm, int * bas, double * env, double ** S, double ** H, double ** two) 
@@ -530,55 +670,61 @@ double * grad(int natm, int nbas, int nshells, int * atm, int * bas, double * en
 
 int main()
 {
-    int natm = 2;
-    int nbas = 2;
+    // int natm = 2;
+    // int nbas = 2;
     
-    int nelec = 2;
-    int nshells = 2;
+    // int nelec = 2;
+    // int nshells = 2;
 
-    int * atm = malloc(sizeof(int) * natm * ATM_SLOTS);
-    int * bas = malloc(sizeof(int) * nbas * BAS_SLOTS);
-    double * env = malloc(sizeof(double) * 100);
+    // int * atm = malloc(sizeof(int) * natm * ATM_SLOTS);
+    // int * bas = malloc(sizeof(int) * nbas * BAS_SLOTS);
+    // double * env = malloc(sizeof(double) * 100);
 
-    FILE * file = fopen("/u/jpmedina/libcint/molecules/h2/sto3g.txt", "r");
-    read_arrays(file, natm, nbas, &atm, &bas, &env);
+    // FILE * file = fopen("/u/jpmedina/libcint/molecules/h2/sto3g.txt", "r");
+    // read_arrays(file, natm, nbas, &atm, &bas, &env);
 
-    // RHF
-    int imax = 20;
-    double conv = 0.000001;
+    // double * ovlp = integral1e(natm, nbas, nshells, atm, bas, env, 0, 0);
+    // print_arr(nshells, 2, ovlp);
 
-    double * P = RHF(natm, nbas, nelec, nshells, atm, bas, env, imax, conv);
+    // double * two = integral2e(natm, nbas, nshells, atm, bas, env, 0);
+    // print_arr(nshells, 4, two);
 
-    printf("P:\n");
-    print_arr(nshells, 2, P);
+    // // RHF
+    // int imax = 20;
+    // double conv = 0.000001;
 
-    double E = energy(natm, nbas, nshells, atm, bas, env, P);
-    printf("E:\n%lf", E);
+    // double * P = RHF(natm, nbas, nelec, nshells, atm, bas, env, imax, conv);
 
-    double * denv = malloc(sizeof(double) * 100);
+    // printf("P:\n");
+    // print_arr(nshells, 2, P);
 
-    double dE = __enzyme_autodiff((void *) energy,
-        enzyme_const, natm,
-        enzyme_const, nbas,
-        enzyme_const, nshells,
-        enzyme_const, atm,
-        enzyme_const, bas, 
-        enzyme_dup, env, denv,
-        enzyme_const, P);
+    // double E = energy(natm, nbas, nshells, atm, bas, env, P);
+    // printf("E:\n%lf", E);
 
-    printf("denv:\n");
-    for (int k = 28; k < 34; k++) {
-        printf("%f ", denv[k]);
-    }
-    printf("\n");
+    // double * denv = malloc(sizeof(double) * 100);
 
-    memset(denv, 0, sizeof(double) * 100);
-    printf("grad:\n");
-    denv = grad(natm, nbas, nshells, atm, bas, env, P);
-    for (int k = 28; k < 34; k++) {
-        printf("%f ", denv[k]);
-    }
-    printf("\n");
+    // double dE = __enzyme_autodiff((void *) energy,
+    //     enzyme_const, natm,
+    //     enzyme_const, nbas,
+    //     enzyme_const, nshells,
+    //     enzyme_const, atm,
+    //     enzyme_const, bas, 
+    //     enzyme_dup, env, denv,
+    //     enzyme_const, P);
+
+    // printf("denv:\n");
+    // for (int k = 28; k < 34; k++) {
+    //     printf("%f ", denv[k]);
+    // }
+    // printf("\n");
+
+    // memset(denv, 0, sizeof(double) * 100);
+    // printf("grad:\n");
+    // denv = grad(natm, nbas, nshells, atm, bas, env, P);
+    // for (int k = 28; k < 34; k++) {
+    //     printf("%f ", denv[k]);
+    // }
+    // printf("\n");
 
     // double E;
     // energy(&E, natm, nbas, nshells, atm, bas, env, P);
@@ -596,24 +742,24 @@ int main()
     // }
     // printf("\n");
 
-    printf("finite diff:\n");
+    // printf("finite diff:\n");
     
-    double grad;
-    double E1, E2;
-    double h = 0.000001;
+    // double grad;
+    // double E1, E2;
+    // double h = 0.000001;
     
-    for (int k = 28; k < 34; k++) {
-        env[k] += h;
-        E1 = energy(natm, nbas, nshells, atm, bas, env, P);
-        env[k] -= 2.0*h;
-        E2 = energy(natm, nbas, nshells, atm, bas, env, P);
-        env[k] += h;
+    // for (int k = 28; k < 34; k++) {
+    //     env[k] += h;
+    //     E1 = energy(natm, nbas, nshells, atm, bas, env, P);
+    //     env[k] -= 2.0*h;
+    //     E2 = energy(natm, nbas, nshells, atm, bas, env, P);
+    //     env[k] += h;
 
-        grad = (E1 - E2)/(2.0*h);
+    //     grad = (E1 - E2)/(2.0*h);
 
-        printf("%lf ", grad);
-    }
-    printf("\n");
+    //     printf("%lf ", grad);
+    // }
+    // printf("\n");
 
     return 0;
 }
