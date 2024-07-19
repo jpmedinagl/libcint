3db258876b86e1d10810674d12ad8906102790ee
JP <jpmedinagl@gmail.com>
2024-07-19 15:12:54 -0400

    adding sample script

--- python/script.py
+++ python/script.py
@@ -3,25 +3,15 @@ import numpy as np
 import pyscf
 
 mol = pyscf.gto.M(atom='''
-                    O   -0.0000000   -0.1113512    0.0000000
-                    H    0.0000000    0.4454047   -0.7830363
-                    H   -0.0000000    0.4454047    0.7830363''',
+                    H 0 0 -0.8
+                    H 0 0 0.8''',
                     basis='sto-3g')
-
-# mol = pyscf.gto.M(atom='''
-#                     H 0 0 -0.8
-#                     H 0 0 0.8''',
-#                     basis='sto-3g')
 
 atm = np.asarray(mol._atm, dtype=np.int32, order='C')
 bas = np.asarray(mol._bas, dtype=np.int32, order='C')
 env = np.asarray(mol._env, dtype=np.double, order='C')
 
-natm = 3
-nbas = 5
-    
-nelec = 10
-nshells = 7
+natm, nbas, nelec, nshells = 2, 2, 2, 2
 
 P = libscf.RHF(natm, nbas, nelec, nshells, atm, bas, env)
 print("P:\n", P)
@@ -31,3 +21,34 @@ print("E:\n", E)
 
 denv = libscf.grad(natm, nbas, nshells, atm, bas, env, P)
 print("GRAD:\n", denv[28:34])
+
+S = libscf.int1e(natm, nbas, nshells, atm, bas, env, 'ovlp')
+print("S:\n", S)
+
+T = libscf.int1e(natm, nbas, nshells, atm, bas, env, 'kin')
+print("T:\n", T)
+
+
+mol = pyscf.gto.M(atom='''
+                    O   -0.0000000   -0.1113512    0.0000000
+                    H    0.0000000    0.4454047   -0.7830363
+                    H   -0.0000000    0.4454047    0.7830363''',
+                    basis='sto-3g')
+
+atm = np.asarray(mol._atm, dtype=np.int32, order='C')
+bas = np.asarray(mol._bas, dtype=np.int32, order='C')
+env = np.asarray(mol._env, dtype=np.double, order='C')
+
+natm, nbas, nelec, nshells = 3, 5, 10, 7
+
+P = libscf.RHF(natm, nbas, nelec, nshells, atm, bas, env)
+print("P:\n", P)
+
+E = libscf.energy(natm, nbas, nshells, atm, bas, env, P)
+print("E:\n", E)
+
+S = libscf.int1e(natm, nbas, nshells, atm, bas, env, 'ovlp')
+print("S:\n", S)
+
+T = libscf.int1e(natm, nbas, nshells, atm, bas, env, 'kin')
+print("T:\n", T)
