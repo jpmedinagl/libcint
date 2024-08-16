4d4cd59b9921b829c435ecfd245cd9d9c178ebf8
JP <jpmedinagl@gmail.com>
2024-08-16 09:58:51 -0400

    update file

--- librint/scf/update.rs
+++ librint/scf/update.rs
@@ -9,60 +9,77 @@ use librint::cint_bas::CINTcgto_cart;
 use librint::cint1e::cint1e_ovlp_cart;
 use librint::cint2e::cint2e_cart;
 
-use librint::scf::{density, energyfast};
+// use librint::scf::{density, energyfast};
+use librint::scf::nmol;
 use librint::utils::{split, combine};
 
 pub const ATM_SLOTS: usize = 6;
 pub const BAS_SLOTS: usize = 8;
 
-#[no_mangle]
-#[autodiff(denergyf, Reverse, Const, Const, Const, Duplicated, Const, Active)]
-pub fn energyf(
-    atm: &mut Vec<i32>,
-    bas: &mut Vec<i32>,
-    env1: &mut Vec<f64>,
-    env2: &mut Vec<f64>,
-    P: &mut Vec<f64>,
-) -> f64 {
-    let mut env: Vec<f64> = combine(&env1, &env2);
-    return energyfast(atm, bas, &mut env, P);
-}
+// #[no_mangle]
+// #[autodiff(dovlp, Reverse, Duplicated, Const, Const, Const, Const, Duplicated)]
+// pub fn ovlp(
+//     out: &mut Vec<f64>, 
+//     shls: [i32; 4], 
+//     atm: &mut Vec<i32>,
+//     bas: &mut Vec<i32>, 
+//     env1: &mut Vec<f64>,
+//     env2: &mut Vec<f64>,
+// ) {
+//     let (natm, nbas) = nmol(atm, bas);
+//     let mut env: Vec<f64> = combine(&env1, &env2);
+//     cint1e_ovlp_cart(out, shls, atm, natm as i32, bas, nbas as i32, &mut env);
+// }
 
-#[no_mangle]
-pub fn gradenergyfast(
-    atm: &mut Vec<i32>,
-    bas: &mut Vec<i32>,
-    env: &mut Vec<f64>,
-    P: &mut Vec<f64>,
-) -> Vec<f64> {
-    let (s1, s2) = split(bas);
 
-    let mut env1: Vec<f64> = env[0..s1].to_vec();
-    let mut env2: Vec<f64> = env[s1..s2].to_vec();
+// #[no_mangle]
+// #[autodiff(denergyf, Reverse, Const, Const, Const, Duplicated, Const, Active)]
+// pub fn energyf(
+//     atm: &mut Vec<i32>,
+//     bas: &mut Vec<i32>,
+//     env1: &mut Vec<f64>,
+//     env2: &mut Vec<f64>,
+//     P: &mut Vec<f64>,
+// ) -> f64 {
+//     let mut env: Vec<f64> = combine(&env1, &env2);
+//     return energyfast(atm, bas, &mut env, P);
+// }
 
-    let mut denv: Vec<f64> = vec![0.0; s2-s1];
-    let _ = denergyf(atm, bas, &mut env1, &mut env2, &mut denv, P, 1.0);
+// #[no_mangle]
+// pub fn gradenergyfast(
+//     atm: &mut Vec<i32>,
+//     bas: &mut Vec<i32>,
+//     env: &mut Vec<f64>,
+//     P: &mut Vec<f64>,
+// ) -> Vec<f64> {
+//     let (s1, s2) = split(bas);
 
-    return denv;
-}
+//     let mut env1: Vec<f64> = env[0..s1].to_vec();
+//     let mut env2: Vec<f64> = env[s1..s2].to_vec();
 
-#[no_mangle]
-pub fn denergyfast(
-    atm: &mut Vec<i32>,
-    bas: &mut Vec<i32>,
-    env: &mut Vec<f64>,
-    P: &mut Vec<f64>,
-) -> Vec<f64> {
-    let denergy = gradenergyfast(atm, bas, env, P);
-    // let dS = dSg(atm, bas, env, P);
+//     let mut denv: Vec<f64> = vec![0.0; s2-s1];
+//     let _ = denergyf(atm, bas, &mut env1, &mut env2, &mut denv, P, 1.0);
 
-    let mut dtotal = vec![0.0; denergy.len()];
-    for i in 0..dtotal.len() {
-        dtotal[i] = denergy[i]; // - 0.5 * dS[i];
-    }
+//     return denv;
+// }
 
-    return dtotal;
-}
+// #[no_mangle]
+// pub fn denergyfast(
+//     atm: &mut Vec<i32>,
+//     bas: &mut Vec<i32>,
+//     env: &mut Vec<f64>,
+//     P: &mut Vec<f64>,
+// ) -> Vec<f64> {
+//     let denergy = gradenergyfast(atm, bas, env, P);
+//     // let dS = dSg(atm, bas, env, P);
+
+//     let mut dtotal = vec![0.0; denergy.len()];
+//     for i in 0..dtotal.len() {
+//         dtotal[i] = denergy[i]; // - 0.5 * dS[i];
+//     }
+
+//     return dtotal;
+// }
 
 fn main() -> io::Result<()>{
     let mut atm: Vec<i32> = Vec::new();
@@ -98,6 +115,35 @@ fn main() -> io::Result<()>{
         println!();
     }
 
+    // let (s1, s2) = split(&mut bas);
+
+    // let mut env1: Vec<f64> = env[0..s1].to_vec();
+    // let mut env2: Vec<f64> = env[s1..s2].to_vec();
+
+    // let mut dbuf;
+    // let mut denv;
+
+    // println!("dovlp");
+    // for i in 0..nbas {
+    //     for j in 0..nbas {
+    //         shls[0] = i as i32;
+    //         shls[1] = j as i32;
+            
+    //         let di = CINTcgto_cart(i, &bas);
+    //         let dj = CINTcgto_cart(j, &bas);
+
+    //         buf = vec![0.0; (di * dj) as usize];
+    //         dbuf = vec![0.0; (di * dj) as usize];
+    //         denv = vec![0.0; env2.len()];
+    //         dovlp(&mut buf, &mut dbuf, shls, &mut atm, &mut bas, &mut env1, &mut env2, &mut denv);
+
+    //         for k in 0..denv.len() {
+    //             print!("{} ", denv[k]);
+    //         }
+    //     }
+    //     println!();
+    // }
+
     println!("repulsion");
     for i in 0..nbas {
         for j in 0..nbas {
@@ -126,18 +172,18 @@ fn main() -> io::Result<()>{
         println!();
     }
 
-    let nelec = 2;
+    // let nelec = 2;
 
-    let mut P = density(&mut atm, &mut bas, &mut env, nelec, 20, 1e-6);
+    // let mut P = density(&mut atm, &mut bas, &mut env, nelec, 20, 1e-6);
 
-    let E = energyfast(&mut atm, &mut bas, &mut env, &mut P);
+    // let E = energyfast(&mut atm, &mut bas, &mut env, &mut P);
 
-    println!("{:?}", P);
-    println!("{}", E);
+    // println!("{:?}", P);
+    // println!("{}", E);
 
-    let denv = denergyfast(&mut atm, &mut bas, &mut env, &mut P);
+    // let denv = denergyfast(&mut atm, &mut bas, &mut env, &mut P);
 
-    println!("{:?}", denv);
+    // println!("{:?}", denv);
     
     Ok(())
 }
